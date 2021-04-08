367cd7a50426d8374630f68f9f8714442c94333f
DevKhaira <devinder.khaira@sky.uk>
2021-04-08 13:04:37 +0100

    add expects

--- features/pageobjects/login.ts
+++ features/pageobjects/login.ts
@@ -4,13 +4,14 @@ import { ILogin } from '../Interfaces/login';
 @injectable()
 export class Login implements ILogin {
    public assertLogin = () => {      
-      $('.login').waitForDisplayed();
-      $('.login').isDisplayed();
+      $('.login').waitForDisplayed({timeoutMsg: 'login is not visible please manual test'});
+      expect($('.login').isDisplayed()).toBeDisplayed;
       return this;
    } 
 
    public assertImageInBackground = () => {
-      const backgroundImage = $('.login').getAttribute('background-image');
+      const backgroundImage = $('.login').getCSSProperty('background-image');
+      expect(backgroundImage.value).toBeDefined();
       return this;
    }
 }
